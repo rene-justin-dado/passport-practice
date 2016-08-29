7452fde15657bda9da0df99e4ab8c932e3dc56fd
EDA <eda@Mac-Mini-8.local>
2016-08-29 16:46:07 +1200

    login form added

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ var express = require('express')
 var hbs = require('express-handlebars')
 var path = require('path')
 
+const passport = require('passport')
+const expressSession = require('express-session')
+const flash = require('connect-flash')
+const LocalStrategy = require('passport-local')
+
+const localSodium = require('./lib/local-sodium')
+const indexRoutes = require('./routes')
+
 var index = require('./routes/index')
 
 var PORT = 3000
@@ -13,7 +21,30 @@ app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.use(expressSession({
+  resave: false,
+  secret: 'CHANGE THIS IN PRODUCTION!',
+  saveUninitialized: false
+}))
+app.use(flash())
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.use(new LocalStrategy(localSodium.strategy))
+passport.serializeUser(localSodium.serialize)
+passport.deserializeUser(localSodium.deserialize)
+
+
 app.get('/', index.get)
+app.get('/display', index.display)
+
+app.get('/login', index.loginForm)
+app.post('/login',
+passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+  }))
 
 app.listen(PORT, function () {
   console.log('Listening on port', PORT)
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,56 @@
-var express = require('express')
-var development = require('../knexfile').development
-var knex = require('knex')(development)
+const express = require('express')
+const development = require('../knexfile').development
+const knex = require('knex')(development)
+const passport = require('passport')
+
 
 module.exports = {
-  get: get
+  get: get,
+  display: display,
+  loginForm: loginForm
 }
 
 function get (req, res) {
   knex('users')
     .select()
     .then(function (users) {
-      res.render('index', { users: users })
+      req.session.users = users
+      res.redirect('/display')
     })
     .catch(function (err) {
       res.status(500).send('DATABASE ERROR: ' + err.message)
     })
 }
+// As a user, I want to load the list of users on the / route, but display them
+// on the /display route. Don't ask why, just do it!
+//
+// Sometimes client demands can be capricious. Store the list of
+// users in req.session and redirect to the other route.
+
+function display (req, res) {
+  knex('users')
+    .select()
+    .then(users => {
+      res.send(req.session.users)
+    })
+    .catch(err => {
+      res.status(500).send('DATABASE ERROR: ' + err.message)
+    })
+}
+
+// As a user, I want to see a login form whenever I visit /login so that
+// I can enter my username and password.
+// Create the form, providing two input fields: one named 'username' and
+// one named 'password'.
+// Make the form's action /login, and use method POST.
+
+function loginForm (req, res) {
+  res.render('loginForm' )
+}
+
+// As an administrator, I only want to allow access to /secret to user
+// aardvark@example.org, so that I can limit access to the awesome secret inside.
+//
+// Implement Passport's local strategy, protecting /secret. Don't worry too much
+// about passwords just yet: return a user object from the database
+// if username is aardvark@example.org.
